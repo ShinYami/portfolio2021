9f66f6df50bc59278fd94223c1c909c6fe7a6a5f
Conti Loris <loriisconti@gmail.com>
2021-06-14 14:47:55 +0200

    added particles

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,18 @@
 (function () {
+  const cursor = document.querySelector(".cursor");
 
-    const cursor = document.querySelector('.cursor');
+  const editCursor = (e) => {
+    const { clientX: x, clientY: y } = e;
+    cursor.style.left = x + "px";
+    cursor.style.top = y + "px";
+  };
 
-    const editCursor = e => {
-          const { clientX: x, clientY: y } = e;
-          cursor.style.left = x + 'px';
-          cursor.style.top = y + 'px';
-    };
-
-    
-    window.addEventListener('mousemove', editCursor);
-
+  window.addEventListener("mousemove", editCursor);
 })();
+
+let content = document.getElementsByTagName("body")[0];
+let darkMode = document.getElementById("dark-change");
+darkMode.addEventListener("click", function () {
+  darkMode.classList.toggle("active");
+  content.classList.toggle("night");
+});
